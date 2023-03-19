f7e78a29a907d5ca21dcc9d990de89dd44a1e5e8
PaulJKathmann <paul.kathmann.98@gmail.com>
2023-03-19 16:55:44 +0100

    adding reducer function

--- app/reducers/tasks.js
+++ app/reducers/tasks.js
@@ -5,3 +5,48 @@ const INITIAL_STATE = {
     createStatus: null,  
     createError: null,
 };
+
+‍export default (state = INITIAL_STATE, action) => {  
+    switch (action.type) {
+        case 'FETCH_TASKS_SUCCESS':      
+        return {        
+            ...state,        
+            status: 'success',        
+            data: action.data,        
+            error: null,      
+        };   
+        case 'FETCH_TASKS_FAILURE':      
+        return {        
+            ...state,        
+            status: 'failure',        
+            error: action.error,      
+        }; 
+        case 'FETCH_TASKS':      
+        return {        
+          ...state,        
+          status: 'loading',        
+          error: null,  
+        };
+        case 'CREATE_TASK':    
+        return {      
+            ...state,      
+            createStatus: 'loading',      
+            createError: null,    
+        };  
+        case 'CREATE_TASK_SUCCESS':    
+        return {      
+            ...state,      
+            createStatus: 'success',      
+            data: [...state.data, action.data],      
+            createError: null,    
+        };  
+        case 'CREATE_TASK_FAILURE':    
+        return {      
+            ...state,      
+            createStatus: 'failure',      
+            createError: action.error,    
+        };
+        default:      
+            return state;
+    }
+};
